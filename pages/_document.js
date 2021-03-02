2caacaef39d9331d6393401bba97608eee1df51e
Jonathan Santiago <36797222+jsantiago611@users.noreply.github.com>
2021-03-02 11:22:50 -0500

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -26,18 +26,18 @@ class MyDocument extends Document {
           <link
             rel="preload"
             as="style"
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
+            <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
           />
           <link
             rel="stylesheet"
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
+            <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
             media="print"
             onLoad="this.media='all'"
           />
           <noscript>
             <link
               rel="stylesheet"
-              href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
+              <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
             />
           </noscript>
           <link
