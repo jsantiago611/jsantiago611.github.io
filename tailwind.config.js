6e45f48109de97f412341a29746edeaa97e683e2
Jonathan Santiago <36797222+jsantiago611@users.noreply.github.com>
2021-03-02 11:23:29 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,7 +21,7 @@ module.exports = {
         14: '3.5rem',
       },
       fontFamily: {
-        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
       colors: {
         blue: colors.lightBlue,
